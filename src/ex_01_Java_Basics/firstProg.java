ce9ca78a51d7f8629bc25717d8a5966a4adc3149
gaya3nav <gayu16695@gmail.com>
2025-05-09 16:38:44 +0530

    First java program-updated

--- src/ex_01_Java_Basics/firstProg.java
+++ src/ex_01_Java_Basics/firstProg.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basics;
 
 public class firstProg {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello all");
     }
 }
