677b410bbc21ca3f60fb92af9ec4f8dbdbfd4122
gaya3nav <gayu16695@gmail.com>
2025-05-19 11:00:58 +0530

    Updted program

---
+++ src/ex_01_Java_Basics/evenOdd.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+public class evenOdd {
+    public static void main(String[] args) {
+        int num = 11;
+        //even or odd
+        String result = (num % 2 == 0) ? "The number is even" : "The number is odd";
+
+        System.out.println(result);
+
+    }
+
+}
